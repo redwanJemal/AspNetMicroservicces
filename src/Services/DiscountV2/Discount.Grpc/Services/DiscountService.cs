5f3c219a2cef2f344efb595f31436cc6be469315
redwanJemal <redjemal2@gmail.com>
2022-06-05 07:46:35 +0530

    Descount Grpc Added

---
+++ src/Services/DiscountV2/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService: DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repoistory;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository repoistory, ILogger<DiscountService> logger, IMapper mapper)
+        {
+            _repoistory = repoistory;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repoistory.GetDiscount(request.ProductName);
+            if(coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount With Product Name {request.ProductName} Not Found"));
+            }
+            _logger.LogInformation("Discount is retrived for Product Name: {ProductName}, Amount {Amount}", coupon.ProductName, coupon.Amount);
+
+            var couponeModel =  _mapper.Map<CouponModel>(coupon);
+            return couponeModel;
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Coupon>(request.Coupon);
+            var newCoupon = await _repoistory.CreateDiscount(coupon);
+
+            _logger.LogInformation("Discount is Successfully Created");
+            var couponeModel = _mapper.Map<CouponModel>(newCoupon);
+
+            return couponeModel;
+        }
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Coupon>(request.Coupon);
+            var updatedCoupon = await _repoistory.UpdateDiscount(coupon);
+
+            _logger.LogInformation("Discount is Successfully Updaated");
+            var couponeModel = _mapper.Map<CouponModel>(updatedCoupon);
+
+            return couponeModel;
+
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var deleted = await _repoistory.DeleteDiscount(request.ProductName);
+            var response = new DeleteDiscountResponse {
+                Success = deleted
+            };
+
+            return response;
+        }
+    }
+}
--- src/Services/DiscountV2/Discount.Grpc/Startup.cs
+++ src/Services/DiscountV2/Discount.Grpc/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace Discount.Grpc
 {
@@ -16,7 +19,10 @@ namespace Discount.Grpc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+            services.AddAutoMapper(typeof(Startup));
             services.AddGrpc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,7 +37,7 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<DiscountService>();
 
                 endpoints.MapGet("/", async context =>
                 {
