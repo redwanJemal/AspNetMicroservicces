753ea518cb653bdaf382ccaf154c429515bb6400
redwanJemal <redjemal2@gmail.com>
2022-06-08 06:41:17 +0530

    Ocelot Api Gateway added

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -18,12 +18,17 @@ namespace OcelotApiGw
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-                .ConfigureLogging((hostingContext, loggingbuilder) => {
-                    loggingbuilder.AddConfiguration(hostingContext.Configuration);
+                .ConfigureLogging((hostingContext, loggingbuilder) =>
+                {
+                    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
                     loggingbuilder.AddConsole();
                     loggingbuilder.AddDebug();
                 });
